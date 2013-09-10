6e3a6f375e9a60e8aab1e1082c51c508934ec4ab
Gayan <gayan@wso2mobile.com>
2013-09-10 09:57:52 +0530

    merging

--- api/policyRouter.js
+++ api/policyRouter.js
@@ -18,34 +18,9 @@ var policy = (function () {
 
         });
 
-        router.put('policy/', function(ctx){
-            log.info("check policy router add permission group PUT");
-            log.info(ctx);
-            var result = policy.addPermissionGroup(ctx);
-            if(result != 'undefined' && result != null){
-                response.status = 200;
-                response.content = result;
-            }else{
-                response.status = 404;
-            }
 
-        });
 
-        router.get('policy/', function(ctx){
-            log.info("check policy router GET");
-            log.info(ctx);
-            var result = policy.getAllPermissionGroups(ctx);
-            log.info(result);
-            if(result != 'undefined' && result != null && result[0] != null){
-                response.status = 200;
-                response.content = result;
-            }else{
-                response.status = 404;
-            }
-
-        });
-
-        router.delete('policy/{policyid}', function(ctx){
+        router.delete('policies/{policyid}', function(ctx){
             policy.delete(ctx);
             response.status = 201;
             return true;
