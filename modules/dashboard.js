8e6809dc5255a7f33411a2e2e31b4d2d63c24d75
Chatura Dilan <dilan@dilan.me>
2013-10-23 17:55:43 +0530

    added no data to dashboard

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -35,6 +35,12 @@ var dashboard = (function () {
           
           	var finalResult = db.query("SELECT platforms.type_name as label, ROUND((count(devices.id)/(select count(id) from devices))*100,0) as data from platforms, devices where devices.platform_id = platforms.id group by type");
                        
+            
+            
+            if(finalResult.length == 0){
+            	finalResult =  [{"label" : "No Data", "data" : 100}];
+            }
+            
             return finalResult;
             
             
@@ -49,8 +55,10 @@ var dashboard = (function () {
 	        var allByodCount = db.query("select count(id) as count from devices where byod=1");
 	        var finalResult =  [{"label" : "Personal", "data" : allByodCount[0].count}, {"label" : "Corporate", "data" : allDeviceCount[0].count - allByodCount[0].count}];   
             
-            
-            
+                       
+             if(finalResult.length == 0){
+            	finalResult =  [{"label" : "No Data", "data" : 100}];
+            }
             
             return finalResult;            
       
