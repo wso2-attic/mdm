bca8b14a94f61d677f6e0b4e6c16a185746ae55c
Gayan <gayan@wso2mobile.com>
2013-11-12 11:42:08 +0530

    merging

--- modules/device.js
+++ modules/device.js
@@ -249,8 +249,8 @@ var device = (function () {
         var users = db.query("SELECT user_id FROM devices WHERE id = ?", ctx.deviceid+"");
         var userId = users[0].user_id;
 
-        var currentdate = new Date();
-        var datetime =  currentdate.getDate() + "/"+ (currentdate.getMonth()+1)  + "/"+ currentdate.getFullYear() + " @ "+ currentdate.getHours() + ":"+ currentdate.getMinutes() + ":"+ currentdate.getSeconds();
+
+        var datetime =  common.getCurrentDateTime();
 
         log.info("Test operation"+ctx.operation);
 
