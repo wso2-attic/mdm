cb670d1bb8d9124cf83eebdc0d44e1873bc95cb5
Gayan <gayan@wso2mobile.com>
2014-01-07 14:30:44 +0530

    merge report router

---
+++ api/reportRouter.js
@@ -0,0 +1,24 @@
+var mdm_reports = (function () {
+
+    var module = function (db,router) {
+        var reportModule = require('modules/mdm_reports.js').mdm_reports;
+        var report = new reportModule(db);
+        router.get('mdm_reports/devices/registration', function(ctx){
+            var reports = report.getDevicesByRegisteredDate(ctx);
+            if(reports[0]!=null){
+                response.content = reports;
+                response.status = 200;
+            }else{
+                response.status = 404;
+            }
+        });
+    };
+    // prototype
+    module.prototype = {
+        constructor: module
+    };
+    // return module
+    return module;
+})();
+
+
