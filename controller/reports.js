16b55b7d5eb206e41e02023be4f142945271697f
Chatura Dilan Perera <dilan@dilan.me>
2014-01-08 12:09:39 +0530

    added second report

--- controller/reports.js
+++ controller/reports.js
@@ -41,3 +41,32 @@ all_reg_devices = function(appController){
 	return context;	
 	
 };
+
+
+devices_complience = function(appController){	
+	
+	var results = null; 
+	
+	if(request.getMethod() == 'POST'){
+		
+		var startdate = request.getParameter('startdate');
+		var enddate = request.getParameter('enddate');
+		var platform = request.getParameter('platform');
+		
+		var reportResults = report.getDevicesByRegisteredDate({startDate: startdate, endDate: enddate, platformType : platform});
+		
+		results = reportResults;
+	}
+	
+		
+	context = appController.context();
+	context.title = context.title + " | Reports";	
+	context.jsFile= "reports/reports.js";
+	context.page = "reports";
+	context.data = {
+		results: results,
+		inputData : {startdate: startdate, enddate: enddate, platform : platform}		
+	};
+	return context;	
+	
+};
