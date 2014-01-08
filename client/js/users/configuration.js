71e4dc595a58e5fa9035a8f11f12a46a5988280c
Chatura Dilan Perera <dilan@dilan.me>
2014-01-08 23:39:01 +0530

    user table width fixed

--- client/js/users/configuration.js
+++ client/js/users/configuration.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
                       null,
                       
                       {                         
+                        "sWidth": "40%",
                         "fnRender": function (oObj)                              
                         {                           
                            
