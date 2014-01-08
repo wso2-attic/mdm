1866ab68a225559bce79442573a353613b204742
Gayan <gayan@wso2mobile.com>
2014-01-08 11:10:02 +0530

    merging

--- api/reportRouter.js
+++ api/reportRouter.js
@@ -4,8 +4,8 @@ var reports = (function () {
         var reportModule = require('modules/mdm_reports.js').mdm_reports;
         var report = new reportModule(db);
         router.get('mdm_reports/devices/registration', function(ctx){
-           // var reports = report.getDevicesByRegisteredDate(ctx);
-            var reports = report.getComplianceStatus(ctx);
+            var reports = report.getDevicesByRegisteredDate(ctx);
+           // var reports = report.getComplianceStatus(ctx);
             if(typeof reports !== 'undefined' && reports !== null && typeof reports[0]!== 'undefined' && reports[0]!== null){
                 response.content = reports;
                 response.status = 200;
