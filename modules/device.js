28f17cae8c70c15a58d8adf7b419ccff91b32a36
Niranjan-K <niranjan.karu@gmail.com>
2014-01-09 13:20:09 +0530

    Permissions

--- modules/device.js
+++ modules/device.js
@@ -495,8 +495,8 @@ var device = (function () {
                 log.info("Test1");
                 // log.info(checkPermission(role,deviceId, featureList[i].name, this));
                 log.info("Test2");
-                //featureArr["enable"] = checkPermission(role,deviceId, featureList[i].name, this);
-                featureArr["enable"] = true;
+                featureArr["enable"] = checkPermission(role,deviceId, featureList[i].name, this);
+                //featureArr["enable"] = true;
                 if(featureList[i].template === null || featureList[i].template === ""){
 
                 }else{
