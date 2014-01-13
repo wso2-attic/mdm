2a906918be430926a6a32afb86295b9142712f15
Gayan <gayan@wso2mobile.com>
2014-01-13 08:58:38 +0530

    change getUsersOfRoleByAssignment function

--- modules/user_group.js
+++ modules/user_group.js
@@ -101,7 +101,7 @@ var user_group = (function () {
                 }
             }
             return array;
-        },
+        },/*
         getUsersOfRoleByAssignment :function(ctx){
             var usersOfGroup = group.getUsersOfGroup(ctx);
             var allUsers = user.getAllUsers(ctx);
@@ -122,6 +122,37 @@ var user_group = (function () {
                 }
             }
             return allUsers;
+        },*/
+        getUsersOfRoleByAssignment :function(ctx){
+            var usersOfGroup = group.getUsersOfGroup(ctx);
+            var allUsers = user.getAllUserNames(ctx);
+            var userArray = new Array();
+            if(usersOfGroup.length==0){
+                for(var i=0;i<allUsers.length;i++){
+                    var obj = {};
+                    obj.available = false;
+                    obj.username = allUsers[i];//can be changed as email
+                    userArray.push(obj);
+                }
+            }else{
+                for(var i=0;i<allUsers.length;i++){
+                    for(var j=0;j<usersOfGroup.length;j++){
+                        if(allUsers[i]==usersOfGroup[j].username){
+                            var obj = {};
+                            obj.available = true;
+                            obj.username = allUsers[i];
+                            userArray.push(obj);
+                            break;
+                        }else{
+                            var obj = {};
+                            obj.available = false;
+                            obj.username = allUsers[i];
+                            userArray.push(obj);
+                        }
+                    }
+                }
+            }
+            return userArray;
         },
         sendEmailToGroup: function(ctx){
             var users = group.getUsersOfGroup(ctx);
