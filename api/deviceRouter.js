81aa8d1601adbaa17ab8ac08d4518ca4989e294b
Niranjan-K <niranjan.karu@gmail.com>
2014-01-16 15:37:20 +0530

    Device validation

--- api/deviceRouter.js
+++ api/deviceRouter.js
@@ -7,6 +7,57 @@ var device = (function () {
 		var device = new deviceModule(db);
 		user = new userModule(db);
 
+
+        var validateDevice = function() {
+
+            //Allow Android version 4.0.3 and above
+            //Allow iOS (iPhone and iPad) version 5.0 and above
+
+            var userOS; //will either be iOS, Android or unknown
+            var userOSversion;  //will be a string, use Number(userOSversion) to convert
+
+            var useragent = arguments[0];
+            var uaindex;
+
+            //determine the OS
+            if(useragent.match(/iPad/i) || useragent.match(/iPhone/i)) {
+                userOS = 'iOS';
+                uaindex = useragent.indexOf('OS ');
+            } else if (useragent.match(/Android/i)) {
+                userOS = 'Android';
+                uaindex = useragent.indexOf('Android ');
+            } else {
+                userOS = 'unknown';
+            }
+
+            //determine version
+            if (userOS == 'iOS' && uaindex > -1) {
+                userOSversion = useragent.substr(uaindex + 3, 3).replace('_', '.');
+            } else if (userOS == 'Android' && uaindex > -1) {
+                userOSversion = useragent.substr(uaindex + 8, 3);
+            } else {
+                userOSversion = 'unknown';
+            }
+
+            if (userOS == 'Android' && userOSversion.substr(0, 4) == '4.0.') {
+                if(Number(userOSversion.charAt(4)) >= 3 ) {
+                    //Allow device
+                    return true;
+                }else {
+                    //Android version not allowed
+                    return false;
+                }
+            } else if (userOS == 'Android' && Number(userOSversion.substr(0,3)) >= 4.1) {
+                //Allow device
+                return true;
+            } else if(userOS == 'iOS' && Number(userOSversion.charAt(0)) >= 5) {
+                //Allow device
+                return true;
+            } else {
+                return false;
+            }
+        }
+
 		router.post('devices/isregistered', function(ctx){
 		    var result = device.isRegistered(ctx);
             log.info(result);
@@ -25,8 +76,10 @@ var device = (function () {
 		router.get('device_enroll', function(ctx){
             var userAgent= request.getHeader("User-Agent");
 
-            if (userAgent.indexOf("Android") > 0) {
-                response.sendRedirect(configs.device.android_location);
+            if (validateDevice(userAgent) == false) {
+                response.sendRedirect("../invaliddevice");
+            } else if (userAgent.indexOf("Android") > 0) {
+                response.sendRedirect("/mdm/androidapk");
             } else if (userAgent.indexOf("iPhone") > 0) {
                 response.sendRedirect(configs.device.ios_location);
             } else if (userAgent.indexOf("iPad") > 0){
@@ -35,9 +88,7 @@ var device = (function () {
                 response.sendRedirect(configs.device.ios_location);
             } else {
                 response.sendRedirect("../invaliddevice");
-                //response.sendRedirect(configs.device.ios_location);
             }
-
 		});
 
 		router.post('devices/register', function(ctx){
