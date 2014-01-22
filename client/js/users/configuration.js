959c29e8d2e96c5722e12488cb3b606c78c19cdc
Chatura Dilan Perera <dilan@dilan.me>
2014-01-22 14:06:22 +0530

    add user type filtering

--- client/js/users/configuration.js
+++ client/js/users/configuration.js
@@ -43,10 +43,16 @@ $(document).ready(function() {
 		
 		"sAjaxSource" : "/mdm/api/webconsole/allUsers",
 		"fnServerParams": function ( aoData ) {
+			
+			var usertype= $('.block-body #userTypeSelect option:selected').val();
           	var roleid = getURLParameter('group');
           	if(roleid != "null"){
           		 aoData.push( { "name": "groupid", "value": roleid } );
-          	}
+            }
+            
+            if(usertype != ""){
+          		  aoData.push( { "name": "userType", "value": usertype } );
+            }
            
            
        },
@@ -74,7 +80,7 @@ $(document).ready(function() {
 
 function fnCreateSelect( aData ){
 
-    var r='<select><option value="">--All--</option><option value="user">User</option><option value="administrator">Administrator</option><option value="mam">MAM</option>', i, iLen=aData.length;
+    var r='<select id="userTypeSelect"><option value="">--All--</option><option value="user">User</option><option value="administrator">Administrator</option><option value="mam">MAM</option>', i, iLen=aData.length;
    // for ( i=0 ; i<iLen ; i++ )
    // {
    //     r += '<option value="'+aData[i]+'">'+aData[i]+'</option>';
