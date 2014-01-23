a93118fa4dac5c851c401ae709bd9c5b60790d91
Chan <dulithaz@gmail.com>
2014-01-23 12:50:13 +0530

    Sending login in authenticate

--- modules/user.js
+++ modules/user.js
@@ -369,12 +369,11 @@ var user = (function () {
 			if(!authStatus) {
 				return null;
 			}
-			var user =  this.getUser({'userid': ctx.username});
+			var user =  this.getUser({'userid': ctx.username, login:true});
 
             var result = db.query(sqlscripts.tenantplatformfeatures.select1,  stringify(user.tenantId));
             if(result[0].record_count == 0) {
 				for(var i = 1; i < 13; i++) {
-
                     var result = db.query(sqlscripts.tenantplatformfeatures.select2, stringify(user.tenantId), i);
 				}
 			}
